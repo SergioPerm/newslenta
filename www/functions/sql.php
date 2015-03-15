396320ccc0a994e694a2eefb51b3a259aa85208b
SergioPerm <itotdel@list.ru>
2015-03-15 12:07:12 +0500

    добавлены классы

--- www/functions/sql.php
+++ www/functions/sql.php
@@ -1,30 +1,54 @@
 <?php
 
-function sql_connect()
-{
-    mysql_connect('localhost','root','');
-    mysql_select_db('newslenta');
-}
+class BDNews {
 
-function sql_query($sql)
-{
-    sql_connect();
-    $res = mysql_query($sql);
-
-    $ret = [];
-
-    while (false !== $row = mysql_fetch_assoc($res)) {
-        $ret[] = $row;
+    public function __construct($host,$user,$pass,$nameBD) {
+        mysql_connect($host,$user,$pass);
+        mysql_select_db($nameBD);
     }
 
-    return $ret;
+    public function receiveData($sqltext) {
+        $res = mysql_query($sqltext);
 
+        $ret = [];
+
+        while (false !== $row = mysql_fetch_assoc($res)) {
+            $ret[] = $row;
+        }
+
+        return $ret;
+    }
+
+    public function execData($sqltext) {
+        mysql_query($sqltext);
+    }
 }
 
-function sql_exec($sql)
-{
-    sql_connect();
-    mysql_query($sql);
-}
+//function sql_connect()
+//{
+//    mysql_connect('localhost','root','');
+//    mysql_select_db('newslenta');
+//}
+//
+//function sql_query($sql)
+//{
+//    sql_connect();
+//    $res = mysql_query($sql);
+//
+//    $ret = [];
+//
+//    while (false !== $row = mysql_fetch_assoc($res)) {
+//        $ret[] = $row;
+//    }
+//
+//    return $ret;
+//
+//}
+//
+//function sql_exec($sql)
+//{
+//    sql_connect();
+//    mysql_query($sql);
+//}
 
 ?>
