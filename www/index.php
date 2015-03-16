8a08330ad33218eb07aa2630aae306fdaff77f3c
SergioPerm <itotdel@list.ru>
2015-03-16 17:12:57 +0500

    добавлены классы

--- www/index.php
+++ www/index.php
@@ -1,11 +1,13 @@
 <?php
 //Фронт контроллер
+require_once __DIR__ . '/autoload.php';
+
 $ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
 $controllerClassName = $ctrl . 'Controller';
 
-require_once __DIR__ . '/controllers/' . $controllerClassName . '.php';
+//require_once __DIR__ . '/controllers/' . $controllerClassName . '.php';
 
 $controllerNews = new $controllerClassName;
 $method = 'action' . $act;
