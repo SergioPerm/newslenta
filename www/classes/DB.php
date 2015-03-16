1d219fcd6a2503414f1973a9b279bab92259212c
SergioPerm <itotdel@list.ru>
2015-03-16 14:47:43 +0500

    добавлены классы

---
+++ www/classes/DB.php
@@ -0,0 +1,34 @@
+<?php
+
+class DB {
+
+    public function __construct()
+    {
+        mysql_connect('localhost','root','');
+        mysql_select_db('news');
+    }
+
+    public function query($sql, $class = 'stdClass')
+    {
+        $res = mysql_query($sql);
+
+        if (false === $res) {
+            return false;
+        }
+
+        $ret = [];
+
+        while ($row = mysql_fetch_object($res, $class)) {
+            $ret[] = $row;
+        }
+        return $ret;
+    }
+
+    public function execQuery($sql)
+    {
+        return mysql_query($sql);
+    }
+
+}
+
+?>
