855ffdb739bdec87d4162e70e971ef14cc4d8fed
SergioPerm <itotdel@list.ru>
2015-04-02 17:53:04 +0500

    добавлены классы

--- www/views/news/all.php
+++ www/views/news/all.php
@@ -3,10 +3,21 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title></title>
+    <style>
+        .radius {
+            background: #fff; /* Цвет фона */
+            border-radius: 5px;
+        }
+        .textlabel {
+            border: 3px solid lightskyblue; /* Параметры рамки */
+            padding: 2px; /* Поля вокруг текста */
+            margin-bottom: 2px; /* Отступ снизу */
+        }
+    </style>
 </head>
 <body>
 
-<table border="1" width="800px">
+<table border="1" width="800px" class="radius">
     <tr>
         <th>Дата и время </th>
         <th>Новость </th>
