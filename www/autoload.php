08d43e8564d875a70e027d921028a6f95ef71245
SergioPerm <itotdel@list.ru>
2015-04-02 17:57:00 +0500

    добавлены классы

--- www/autoload.php
+++ www/autoload.php
@@ -1,17 +1,12 @@
 <?php
-
 function __autoload($class)
 {
     if (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
         require __DIR__ . '/controllers/' . $class . '.php';
-<<<<<<< HEAD
-=======
     } elseif (file_exists(__DIR__ . '/classes/' . $class . '.php')) {
         require __DIR__ . '/classes/' . $class . '.php';
     } elseif (file_exists(__DIR__ . '/models/' . $class . '.php')) {
         require __DIR__ . '/models/' . $class . '.php';
->>>>>>> f376708dddf2b2339154e5ec3c7a14a8c6c5be0a
     }
 }
-
 ?>
