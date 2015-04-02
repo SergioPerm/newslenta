760b6c3f699801a3588c0a0b65d7aed7799f239a
SergioPerm <itotdel@list.ru>
2015-04-02 17:53:09 +0500

    добавлены классы

--- www/views/form.php
+++ www/views/form.php
@@ -1,7 +1,7 @@
 <form action="../index.php" method="post">
     <label for="title">Заголовок новости</label>
-    <input type="text" id="title" name="title"><br/>
+    <input type="text" id="title" name="title" class="radius textlabel"><br/>
     <label for="content">Новость</label>
-    <input type="text" id="content" name="content">
+    <input type="text" id="content" name="content" class="radius textlabel">
     <input type="submit">
 </form>
