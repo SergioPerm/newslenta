013d18f9df81d94413f2682842c35e9f474b01f0
SergioPerm <itotdel@list.ru>
2015-04-14 17:40:59 +0600

    123

--- www/models/news.php
+++ www/models/news.php
@@ -1,44 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../classes/DB.php';
 
-function sortByDateTime($a,$b)
-{
-    if ($a['datetime'] < $b['datetime']) {
-        return 1;
-    } else {
-        return 0;
-    }
+class News extends AbstractModel {
 
-}
-
-class News {
     public $id;
     public $title;
     public $content;
 
-    public static function getAll()
-    {
-        $db = new DB;
-        return $db->queryAll('SELECT * FROM news ORDER BY datetime DESC', 'News');
-    }
-
-    public static function insert_news($data)
-    {
-        $sqltext =
-        "INSERT INTO news
-        (datetime,title,content)
-        VALUES
-        (NOW(),'" . $data['title'] . "','" . $data['content'] . "')";
-
-        $db = new DB;
-        return $db->execQuery($sqltext);
-    }
-
-    public static function getOne($id)
-    {
-        $db = new DB();
-        return $db->queryOne('SELECT * FROM news WHERE id=' . $id, 'News');
-    }
+    protected static $table = 'news';
+    protected static $class = 'News';
 }
 ?>
