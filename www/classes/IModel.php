0ecc6773732641c1e582558f15e1b42f7b14501d
SergioPerm <itotdel@list.ru>
2015-04-14 19:00:40 +0600

    добавлены классы

---
+++ www/classes/IModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kluv
+ * Date: 14.04.2015
+ * Time: 18:29
+ */
+
+interface IModel {
+
+    public static function getAll();
+    public static function getOne($id);
+
+} 
