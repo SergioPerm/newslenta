7dfd5ed7a2c6f3f99f729fec8c777d2833c90399
SergioPerm <itotdel@list.ru>
2015-04-14 19:00:46 +0600

    добавлены классы

--- www/classes/View.php
+++ www/classes/View.php
@@ -1,17 +1,26 @@
 <?php
 
-class View {
+class View
+    implements Iterator
+{
 
+    protected $_position = 0;
     protected $data = [];
 
-    public function assign($name, $value)
+    public function __construct()
     {
-        $this->data[$name] = $value;
+        $this->_position = 0;
     }
 
     public function __set($k,$v)
     {
+        $this->data[$k] = $v;
+    }
 
+    public function __get($k)
+    {
+        //магический метод php для автоматического чтения недоступного свойства объекта...
+        return $this->data[$k];
     }
 
     public function display($template)
@@ -22,4 +31,28 @@ class View {
         include __DIR__ . '/../views/' . $template;
     }
 
-} 
+    public function current()
+    {
+        return $this->data[$this->_position];
+    }
+
+    public function next()
+    {
+        ++$this->_position;
+    }
+
+    public function key()
+    {
+        return $this->_position;
+    }
+
+    public function valid()
+    {
+        return isset($this->data[$this->_position]);
+    }
+
+    public function rewind()
+    {
+        $this->_position = 0;
+    }
+}
