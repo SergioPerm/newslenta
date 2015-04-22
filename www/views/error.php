4af557ac2dd0f08aca4b3ca1aeab187ba14cefdf
SergioPerm <itotdel@list.ru>
2015-04-22 12:31:38 +0600

    вьюха исключения

---
+++ www/views/error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Ошибка!!!!</title>
+</head>
+<body>
+<h1>ВНИМАНИЕ ОШИБКА!!!</h1></br>
+<h4><?php echo $error; ?></h4>
+</body>
+</html>
