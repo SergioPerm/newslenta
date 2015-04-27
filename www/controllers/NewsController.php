710e7e8c2908cf280378ad1b23b31ac696488961
SergioPerm <itotdel@list.ru>
2015-04-27 20:25:25 +0600

    вьюха исключения

--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -3,20 +3,20 @@ class NewsController {
     public function actionAll()
     {
 
-            $art = NewsModel::findOneByColumn('title','Новый заголо23232323вок123123123!');
+//            $art = NewsModel::findOneByColumn('title','Новый заголо23232323вок123123123!');
 
 
 
 
-//        $article = new NewsModel();
-//
-////        var_dump($article::findByColumn('title','213123123'));die;
-//
-//        $news = $article->findAll();
-//
-//        $view = new View();
-//        $view->items = $news;
-//        $view->display('news/all.php');
+        $article = new NewsModel();
+
+//        var_dump($article::findByColumn('title','213123123'));die;
+
+        $news = $article->findAll();
+
+        $view = new View();
+        $view->items = $news;
+        $view->display('news/all.php');
     }
     public function actionOne()
     {
